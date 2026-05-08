4d5f82c7badc594b5722874f237789bb169cc9a4
richatnort <71719591+richatnort@users.noreply.github.com>
2026-05-08 12:22:05 +0100

    feat: migration SQL for activity flag columns

---
+++ days-out-code/db/migrations/0001_add_activity_flags.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "activities" ADD COLUMN "is_considering" boolean DEFAULT false NOT NULL;
+ALTER TABLE "activities" ADD COLUMN "is_plan" boolean DEFAULT false NOT NULL;
+ALTER TABLE "activities" ADD COLUMN "is_mystery" boolean DEFAULT false NOT NULL;
